6b08050f060f58767eede1a25c20f990d7e717a1
Paul Newland <paulnewland@hotmail.com>
2016-05-26 01:55:29 +0000

    initial commit bootstrap set up in rails sb admin

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+    def bootstrap_elements
+    end
+    
+    def blank_page
+    end
+    
+    def bootstrap_grid
+    end
+    
+    def forms
+    end
+    
+    def index
+    end
+    
+    def tables
+    end
+end
